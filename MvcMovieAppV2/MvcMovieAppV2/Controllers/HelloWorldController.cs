49d11403ab6b546528e12e19971954b41be3d1f4
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-13 07:58:03 -0500

    matches URL segment with the route parameter id

--- MvcMovieAppV2/MvcMovieAppV2/Controllers/HelloWorldController.cs
+++ MvcMovieAppV2/MvcMovieAppV2/Controllers/HelloWorldController.cs
@@ -16,10 +16,14 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public string Welcome(string name, int ID = 1)
         {
-            //return "This is the Welcome action method...";
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
