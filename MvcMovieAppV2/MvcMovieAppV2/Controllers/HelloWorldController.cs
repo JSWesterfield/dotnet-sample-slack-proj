edd92753f733b0fba1113edf8e0cec02671f545e
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-13 01:30:25 -0500

    adds in new mvc .net core project structure

---
+++ MvcMovieAppV2/MvcMovieAppV2/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
+
+// code created when  added a controller
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace MvcMovieAppV2.Controllers
+//{
+//    public class HelloWorldController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
---
+++ MvcMovieAppV2/MvcMovieAppV2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcMovieAppV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
