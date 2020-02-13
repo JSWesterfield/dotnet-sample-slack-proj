f2daf0540c7962b71de7d7fba75c57b26e27f74a
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-13 08:24:29 -0500

    changes wWelcome metthod to add a message and NummTimes value to the ViewData dictionary

--- MvcMovieAppV2/MvcMovieAppV2/Controllers/HelloWorldController.cs
+++ MvcMovieAppV2/MvcMovieAppV2/Controllers/HelloWorldController.cs
@@ -5,43 +5,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
         public IActionResult Index()
         {
             return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        //public string Welcome(string name, int numTimes = 1)
-        //{
-        //    //return "This is the Welcome action method...";
-        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-        //}
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
-
-// code created when  added a controller
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-
-//namespace MvcMovieAppV2.Controllers
-//{
-//    public class HelloWorldController : Controller
-//    {
-//        public IActionResult Index()
-//        {
-//            return View();
-//        }
-//    }
-//}
